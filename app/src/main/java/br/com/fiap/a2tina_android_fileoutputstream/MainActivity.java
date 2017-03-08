db8dd7f07d78c41175212e3e954a9965d04b797d
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-08 11:23:51 -0300

    Criado metodo criar arquivo txts

--- app/src/main/java/br/com/fiap/a2tina_android_fileoutputstream/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_fileoutputstream/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.fiap.a2tina_android_fileoutputstream;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void criar(View view) {
+        //Tratamos a excecao por que vamos tentar escrever algo e da excecao
+        try{
+            FileOutputStream fos = openFileOutput("teste.txt", MODE_PRIVATE);
+            String texto = "Bom dia !!";
+            fos.write(texto.getBytes());
+            fos.close();
+        }catch (Exception e){
+            Toast.makeText(this, "Erro Inesperado!", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
     public void ler(View view) {
